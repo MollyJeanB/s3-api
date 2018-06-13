fac20f436727235cd5b270944122e5aca1828592
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-06-13 12:12:12 -0700

    syntax fix

--- Stackery/functions/retrieveFromS3/index.js
+++ Stackery/functions/retrieveFromS3/index.js
@@ -1,47 +1,43 @@
-
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-module.exports = async request => {
 
-  const ports = JSON.parse(process.env.STACKERY_PORTS)
+module.exports = async request => {
+  const ports = JSON.parse(process.env.STACKERY_PORTS);
+
   // Log the request to the console.
   console.dir(request);
 
   // Get the message sent in the endpoint path parameter
   let fileId = request.pathParameters.fileId;
 
+
   let params = {
     Key: `${fileId}.txt`,
     Bucket: ports[0][0].bucket
   };
+  let responseBody = '';
 
   try {
     let s3Return = await s3.getObject(params);
+    responseBody = s3Return.Body;
     console.dir({
-      status: "success",
-      return: s3Return
-    })
+      status: 'success',
+      return: responseBody
+    });
   } catch (error) {
     console.error(error.message);
   }
 
+  let response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: responseBody
+  };
 
-  // Build a response.
-  // let responseBody = `
-  //   <h4>You sent the following message:</h4>
-  //   <p>${message}</p>
-  // `;
-  //
-  // let response = {
-  //   statusCode: 200,
-  //   headers: {
-  //     "Content-Type": "text/html"
-  //   },
-  //   body: responseBody
-  // };
-  //
-  // return response;
-
-  return {};
+  return {
+    response
+  };
 };
